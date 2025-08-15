e15ab265e38a11345db1cde6ed5dbf4ba640e6b1
Antiqueintellegence <theantiqueintellegence@gmail.com>
2025-08-15 04:56:53 -0700

    Updated tsconfig.json

--- src/services/firebase/config.ts
+++ src/services/firebase/config.ts
@@ -1,27 +1,26 @@
-import { Platform } from "react-native";
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
-import { getFunctions } from "firebase/functions";
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+import { getFunctions } from 'firebase/functions';
+import { getAnalytics } from "firebase/analytics";
+
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAlpKG7P2aQF9b4Y8GVj0qUmWQ42qqZ8oI",
-  authDomain: "ready9jamark.firebaseapp.com",
-  projectId: "ready9jamark",
-  storageBucket: "ready9jamark.firebasestorage.app",
-  messagingSenderId: "1071140466108",
-  appId: "1:1071140466108:web:07b903c705f0d3ceda8fb9",
-  measurementId: "G-PFJ1Q4NZCB"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
-
+    
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
-if (Platform.OS === 'web') {
-  const { getAnalytics } = require("firebase/analytics");
-  const analytics = getAnalytics(app);
-}
-
+// Initialize Firebase services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
